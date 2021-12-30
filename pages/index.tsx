d39492ef8fdd28a425dffd1b3d4759318b410100
claykaufmann <claykaufmann@gmail.com>
2021-12-29 22:39:29 -0800

    remove background style, fix footer, and email

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,49 +3,52 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Clay Kaufmann Photography</title>
-        <meta
-          name="description"
-          content="Photography portfolio of Clay Kaufmann"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Clay Kaufmann Photography</title>
+				<meta
+					name="description"
+					content="Photography portfolio of Clay Kaufmann"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Site under construction...</h1>
-        <p>Soon to be my photography portfolio...</p>
-        <p>
-          In the meantime, check me out on{' '}
-          <a
-            className={styles.link}
-            target="_blank"
-            href="https://www.instagram.com/byclaykay/"
-            rel="noreferrer"
-          >
-            instagram!
-          </a>
-        </p>
-        <p>
-          Or visit{' '}
-          <a
-            className={styles.link}
-            target="_blank"
-            href="https://claykaufmann.com"
-            rel="noreferrer"
-          >
-            my main site!
-          </a>
-        </p>
-      </main>
+			<main className={styles.main}>
+				<h1 className={styles.title}>Site under construction...</h1>
+				<p>Soon to be my photography portfolio...</p>
+				<p>
+					In the meantime, check me out on{' '}
+					<a
+						className={styles.link}
+						target="_blank"
+						href="https://www.instagram.com/byclaykay/"
+						rel="noreferrer"
+					>
+						instagram!
+					</a>
+				</p>
+				<p>
+					Visit{' '}
+					<a
+						className={styles.link}
+						target="_blank"
+						href="https://claykaufmann.com"
+						rel="noreferrer"
+					>
+						my main site!
+					</a>
+				</p>
+				<p>
+					Or get in touch with me via email at <a className={styles.link} href="mailto:me@claykaufmann.com">me@claykaufmann.com</a>
+				</p>
+			</main>
 
-      <footer className={styles.footer}>
-        <p>Site built by Clay Kaufmann</p>
-      </footer>
-    </div>
-  )
+			<footer className={styles.footer}>
+				<p>Site built by Clay Kaufmann</p>
+			</footer>
+		</div>
+	)
 }
 
 export default Home
