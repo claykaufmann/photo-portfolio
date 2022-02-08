c77f94c0d926eeaf9e5cfa7044912a275906cba5
claykaufmann <claykaufmann@gmail.com>
2022-02-07 22:27:49 -0500

    Have all pages use globals scss file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
