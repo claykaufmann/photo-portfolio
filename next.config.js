50692091f3d6a68c959b3bc7c7dd0322696163f4
Clay Kaufmann <claykaufmann@gmail.com>
2022-03-30 16:51:51 -0400

    change image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['photo-website-45hjfg734hgf.s3.us-east-2.amazonaws.com'],
+    domains: ['bucket-tupslv.s3.us-east-2.amazonaws.com'],
   },
 }
