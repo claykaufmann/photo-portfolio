2a95a2559137490b79382f0dfb0854b2804b6384
Clay Kaufmann <claykaufmann@gmail.com>
2022-03-31 17:44:36 -0400

    update home page to have bg image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,52 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Heading, Stack, Center } from '@chakra-ui/react'
+import { Heading, Stack, Center, Box } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import Base from '../components/layout/Base'
+import Header from '../components/layout/Header'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <Base>
+    <div className={styles.splash}>
       <Head>
         <title>Clay Kaufmann Photography</title>
       </Head>
 
-      <Center height="50vh">
-        <Stack
-          paddingTop={['7rem', '9rem', '5rem', '12rem']}
-          direction={['column', 'column', 'row']}
-          spacing={['5rem', '7rem', '10rem', '15rem']}
-        >
-          <NextLink href="/portfolio">
-            <Heading
-              size="xl"
-              cursor="pointer"
-              _hover={{ color: 'gray' }}
-              _activeLink={{ textDecoration: 'line-through' }}
-            >
-              PORTFOLIO.
-            </Heading>
-          </NextLink>
+      <Box margin="0.5em 2em">
+        <Header textColor="white" />
 
-          <NextLink href="/film">
-            <Heading size="xl" cursor="pointer" _hover={{ color: 'gray' }}>
-              FILM.
-            </Heading>
-          </NextLink>
+        <Center height="50vh" color="white" textShadow="1px 1px 1px #3c5c5e">
+          <Stack
+            paddingTop={['7rem', '9rem', '5rem', '12rem']}
+            direction={['column', 'column', 'row']}
+            spacing={['5rem', '7rem', '10rem', '15rem']}
+          >
+            <NextLink href="/portfolio">
+              <Heading
+                size="xl"
+                cursor="pointer"
+                _hover={{ color: 'gray' }}
+                _activeLink={{ textDecoration: 'line-through' }}
+              >
+                PORTFOLIO.
+              </Heading>
+            </NextLink>
 
-          <NextLink href="/about">
-            <Heading size="xl" cursor="pointer" _hover={{ color: 'gray' }}>
-              ABOUT.
-            </Heading>
-          </NextLink>
-        </Stack>
-      </Center>
-    </Base>
+            <NextLink href="/film">
+              <Heading size="xl" cursor="pointer" _hover={{ color: 'gray' }}>
+                FILM.
+              </Heading>
+            </NextLink>
+
+            <NextLink href="/about">
+              <Heading size="xl" cursor="pointer" _hover={{ color: 'gray' }}>
+                ABOUT.
+              </Heading>
+            </NextLink>
+          </Stack>
+        </Center>
+      </Box>
+    </div>
   )
 }
 
