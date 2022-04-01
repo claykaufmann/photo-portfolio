bde90850b6676de7e7334634d48ccc805b9c54cb
Clay Kaufmann <claykaufmann@gmail.com>
2022-04-01 01:24:50 -0400

    redo header to look better on home page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,19 +5,51 @@ import Nav from './Nav'
 
 interface props {
   textColor: string
+  isHomePage?: boolean
 }
 
-const Header: React.VFC<props> = ({ textColor }) => {
-  return (
-    <HStack justifyContent="space-between" color={textColor}>
+const Header: React.VFC<props> = ({ textColor, isHomePage }) => {
+  let spacing = ''
+  let background = ''
+  let stack = <React.Fragment></React.Fragment>
+
+  const innerHeader = (
+    <React.Fragment>
       <NextLink href="/">
-        <Heading size="2xl" cursor="pointer" _hover={{ color: 'gray' }}>
+        <Heading
+          size="2xl"
+          cursor="pointer"
+          _hover={{ textDecoration: 'line-through' }}
+        >
           BYCLAYKAY.
         </Heading>
       </NextLink>
       <Nav textColor={textColor} />
-    </HStack>
+    </React.Fragment>
   )
+
+  if (isHomePage) {
+    spacing = '0.5em 2em'
+    background = 'rgba(255,255,255,0.8)'
+    stack = (
+      <HStack
+        justifyContent="space-between"
+        color={textColor}
+        background={background}
+        padding={spacing}
+      >
+        {innerHeader}
+      </HStack>
+    )
+  } else {
+    stack = (
+      <HStack justifyContent="space-between" color={textColor}>
+        {innerHeader}
+      </HStack>
+    )
+  }
+
+  return <React.Fragment>{stack}</React.Fragment>
 }
 
 export default Header
