d3f9024ec34dc5fd9b5c299e0a0fab6d6aef6665
Clay Kaufmann <claykaufmann@gmail.com>
2022-04-01 17:34:47 -0400

    clean up header component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,47 +6,21 @@ import utilStyles from '../../styles/utils.module.scss'
 
 interface props {
   textColor: string
-  isHomePage?: boolean
 }
 
-const Header: React.VFC<props> = ({ textColor, isHomePage }) => {
-  let spacing = ''
-  let background = ''
-  let stack = <React.Fragment></React.Fragment>
-
-  const innerHeader = (
+const Header: React.VFC<props> = ({ textColor }) => {
+  return (
     <React.Fragment>
-      <NextLink href="/">
-        <Heading size="2xl" cursor="pointer">
-          <span className={utilStyles.strikeThroughLink}>BYCLAYKAY.</span>
-        </Heading>
-      </NextLink>
-      <Nav textColor={textColor} />
+      <HStack justifyContent="space-between" color={textColor}>
+        <NextLink href="/">
+          <Heading size="2xl" cursor="pointer">
+            <span className={utilStyles.strikeThroughLink}>BYCLAYKAY.</span>
+          </Heading>
+        </NextLink>
+        <Nav textColor={textColor} />
+      </HStack>
     </React.Fragment>
   )
-
-  if (isHomePage) {
-    spacing = '0.5em 2em'
-    background = 'rgba(255,255,255,0.8)'
-    stack = (
-      <HStack
-        justifyContent="space-between"
-        color={textColor}
-        background={background}
-        padding={spacing}
-      >
-        {innerHeader}
-      </HStack>
-    )
-  } else {
-    stack = (
-      <HStack justifyContent="space-between" color={textColor}>
-        {innerHeader}
-      </HStack>
-    )
-  }
-
-  return <React.Fragment>{stack}</React.Fragment>
 }
 
 export default Header
