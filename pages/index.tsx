4709dc2269d9b1c21eedf2ef96cbc57090767bc4
Clay Kaufmann <claykaufmann@gmail.com>
2022-04-01 17:35:02 -0400

    style index page better

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Heading, Stack, Center, Box } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import Header from '../components/layout/Header'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -12,22 +11,25 @@ const Home: NextPage = () => {
         <title>Clay Kaufmann Photography</title>
       </Head>
 
-      <Header textColor="black" isHomePage={true} />
-
       <Box margin="0.5em 2em">
+        <NextLink href="/">
+          <Heading
+            size="2xl"
+            color="white"
+            textShadow="1px 1px 1px #3c5c5e"
+            cursor="pointer"
+          >
+            BYCLAYKAY.
+          </Heading>
+        </NextLink>
         <Center height="50vh" color="white" textShadow="1px 1px 1px #3c5c5e">
           <Stack
             paddingTop={['7rem', '9rem', '5rem', '12rem']}
             direction={['column', 'column', 'row']}
-            spacing={['5rem', '7rem', '10rem', '15rem']}
+            spacing={['5rem', '5rem', '7rem', '15rem']}
           >
             <NextLink href="/portfolio">
-              <Heading
-                size="xl"
-                cursor="pointer"
-                _hover={{ color: 'gray' }}
-                _activeLink={{ textDecoration: 'line-through' }}
-              >
+              <Heading size="xl" cursor="pointer" _hover={{ color: 'gray' }}>
                 PORTFOLIO.
               </Heading>
             </NextLink>
