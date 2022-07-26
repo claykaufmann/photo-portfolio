bda7dc601bc4883fa861a92530dc99c9e2e3f991
Clay Kaufmann <claykaufmann@gmail.com>
2022-07-25 21:26:46 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RH4QJFQ3PT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ChakraProvider>
   )
